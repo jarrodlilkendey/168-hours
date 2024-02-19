aabf7d99c7bdf50975e0a92cd280c22b57b3e0ac
Jarrod Lilkendey <programmablewealth@gmail.com>
2024-02-19 13:33:12 +1100

    added a time tracker page

---
+++ components/tracker/TimeTracker.tsx
@@ -0,0 +1,7 @@
+export default function TimeTracker() {
+    return (
+        <div>
+            <h1>Time Tracker</h1>
+        </div>
+    )
+}
---
+++ pages/track/index.tsx
@@ -0,0 +1,11 @@
+import TimeTracker from '@/components/tracker/TimeTracker'
+
+export default function TimeTrackerPage() {
+    return (
+        <div>
+            <TimeTracker />
+        </div>
+    )
+}
+
+TimeTrackerPage.auth = true
