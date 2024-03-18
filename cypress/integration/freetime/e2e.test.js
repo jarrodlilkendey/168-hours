1b0ef0b9c4b1caac70d0e3be2866512d7af52978
Jarrod Lilkendey <programmablewealth@gmail.com>
2024-03-18 12:34:05 +1100

    added e2e tests for freetime calculator

--- cypress/integration/freetime/e2e.test.js
+++ cypress/integration/freetime/e2e.test.js
@@ -5,3 +5,99 @@ it('navigate to free time calculator from home', () => {
     })
     cy.findByRole('heading', { name: 'Free Time Calculator' }).should('exist')
 })
+
+it('update all fields with valid data and calculate free time', () => {
+    cy.visit('/freetime')
+
+    cy.findByLabelText(/Age/i).clear().type(40)
+
+    cy.findByLabelText(/Sleeping/i)
+        .clear()
+        .type(10)
+
+    cy.findByLabelText(/Working/i)
+        .clear()
+        .type(10)
+
+    cy.findByLabelText(/Commuting/i)
+        .clear()
+        .type(10)
+
+    cy.findByLabelText(/Exercise/i)
+        .clear()
+        .type(10)
+
+    cy.findByLabelText(/Eating\/cooking/i)
+        .clear()
+        .type(10)
+
+    cy.findByLabelText(/Chores/i)
+        .clear()
+        .type(10)
+
+    cy.findByLabelText(/Grooming\/hygiene/i)
+        .clear()
+        .type(10)
+
+    cy.findByLabelText(/Parenting duties/i)
+        .clear()
+        .type(10)
+
+    cy.findByLabelText(/Other/i).clear().type(10)
+
+    cy.findByRole('button', { name: 'Calculate Your Free Time' }).click()
+
+    cy.findByRole('heading', { name: 'Results' }).should('exist')
+
+    cy.findByText(/Free time hours per week/i).should('exist')
+
+    cy.findByText(/Free time hours per day/i).should('exist')
+})
+
+it('update enter invalid data and calculate free time', () => {
+    cy.visit('/freetime')
+
+    cy.findByLabelText(/Sleeping/i)
+        .clear()
+        .type(100)
+
+    cy.findByLabelText(/Working/i)
+        .clear()
+        .type(100)
+
+    cy.findByLabelText(/Commuting/i)
+        .clear()
+        .type(100)
+
+    cy.findByLabelText(/Exercise/i)
+        .clear()
+        .type(100)
+
+    cy.findByLabelText(/Eating\/cooking/i)
+        .clear()
+        .type(100)
+
+    cy.findByLabelText(/Chores/i)
+        .clear()
+        .type(100)
+
+    cy.findByLabelText(/Grooming\/hygiene/i)
+        .clear()
+        .type(100)
+
+    cy.findByLabelText(/Parenting duties/i)
+        .clear()
+        .type(100)
+
+    cy.findByLabelText(/Other/i).clear().type(100)
+
+    cy.findByRole('button', { name: 'Calculate Your Free Time' }).click()
+
+    cy.findByRole('heading', { name: 'Results' }).should('exist')
+
+    cy.findByText(/Free time hours per week/i).should('not.exist')
+
+    cy.findByText(/Free time hours per day/i).should('not.exist')
+
+    cy.findByText(/You have entered invalid numbers./i).should('exist')
+})
