fa19d4f2df35d49ce78fbcc11788e649ceb617a2
Jarrod Lilkendey <programmablewealth@gmail.com>
2024-05-23 13:48:40 +1000

    keep going

--- docker-bootstrap-app.sh
+++ docker-bootstrap-app.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-DATABASE_URL="postgres://postgres:postgres@db:5432/appdb?sslmode=disable" npm run prisma:migrate:deploy
-DATABASE_URL="postgres://postgres:postgres@db:5432/workler?sslmode=disable" node server.js
+npx prisma migrate deploy
+node server.js
 # npx prisma migrate deploy
 # node server.js
 # ENVIRONMENT from docker-compose.yaml doesn't get through to subprocesses
