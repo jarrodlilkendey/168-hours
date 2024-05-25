ca9a65af5348a17e398538ea3cd0dd2fe59ae585
Jarrod Lilkendey <programmablewealth@gmail.com>
2024-05-25 19:02:37 +1000

    revert

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -19,15 +19,13 @@ declare global {
 // eslint-disable-next-line import/no-mutable-exports
 let prismaClient: PrismaClient
 
-if (typeof window === 'undefined') {
-    if (process.env.NODE_ENV === 'production') {
-        prismaClient = new PrismaClient()
-    } else {
-        if (!global.prisma) {
-            global.prisma = new PrismaClient()
-        }
-        prismaClient = global.prisma
+if (process.env.NODE_ENV === 'production') {
+    prismaClient = new PrismaClient()
+} else {
+    if (!global.prisma) {
+        global.prisma = new PrismaClient()
     }
+    prismaClient = global.prisma
 }
 
 export default prismaClient
